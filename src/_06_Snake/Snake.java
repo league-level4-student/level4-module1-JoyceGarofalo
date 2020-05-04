f927ca2e5cc41fda6cf852f4c03af1c9f0172d67
joycegarofalo <joycegarofalo@joyces-macbook.attlocal.net>
2020-05-03 17:28:48 -0700

    snake game ip

--- src/_06_Snake/Snake.java
+++ src/_06_Snake/Snake.java
@@ -36,15 +36,33 @@ public class Snake {
 	public void update() {
 		//1. use a switch statement to check on the currentDirection
 		//   of the snake and calculate its next x and y position.
-		
+		switch(currentDirection) {
+		case RIGHT:{
+			head.getLocation().x = head.getLocation().x+1;
+		}
+		case LEFT:{
+			head.getLocation().x = head.getLocation().x-1;
+		}
+		case UP:{
+			head.getLocation().y = head.getLocation().y-1;
+		}
+		case DOWN:{
+			head.getLocation().y = head.getLocation().y+1;
+		}
+		}
 
 		//2. Iterate through the SnakeSegments in reverse order
 		//2a. Update each snake segment to the location of the segment 
 		//    in front of it.
-		
+		for (int i = snake.size(); i > 0; i--) {
+			snake.get(i).setLocation(snake.get(i+1).getLocation()); 
+			//snake.lastIndexOf(snake);
+			head.setLocation(snake.get(i).getLocation());
+			canMove = true;
+
+		} //////////////////////////////////////////////////////////////////////////
 		
 		//3. set the location of the head to the new location calculated in step 1
-		
 
 		//4. set canMove to true
 		
@@ -54,16 +72,23 @@ public class Snake {
 		//1. set the current direction equal to the passed in Direction only if canMove is true.
 		//   set canMove equal to false.
 		//   make sure the snake cannot completely reverse directions.
-		
+		if(canMove == true) {
+			currentDirection = d;
+		}
+		canMove = false;
+		if(currentDirection == Direction.UP) {
+			currentDirection != Direction.DOWN;
+			/////////////////////////////////////////////////////////////////////////
+		}
 	}
 
 	public void reset(Location loc) {
 		//1. clear the snake
-		
+		snake.clear();
 		//2. set the location of the head
-		
+		head.setLocation(loc);
 		//3. add the head to the snake
-		
+		snake.add(head);
 	}
 
 	public boolean isOutOfBounds() {
